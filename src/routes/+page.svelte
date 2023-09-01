7d4aee8370a54f08862e61a04dd1188ed52c6e8f
Rik <rik.de.kort@hey.com>
2023-09-01 11:48:50 +0200

    Use homegrown grid instead of ag-grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,25 @@
 <script lang="ts">
     import {onMount} from "svelte";
     import {loadPyodide} from "pyodide";
-    import AgGrid from "$lib/AgGrid.svelte";
+
+    import Grid from "$lib/Grid.svelte";
     import CodeMirror from "svelte-codemirror-editor";
     import {python} from "@codemirror/lang-python";
-
-    function asData(variables: string[], values: any[][]) {
-        let result = [];
-        for (const [i, variable] of variables.entries()) {
-            for (const [j, value] of values[i].entries()) {
-                if (result.length > j) {
-                    result[j][variable] = value;
-                } else {
-                    result.push({[variable]: value})
-                }
-            }
-        }
-        return result;
-    }
-
-    function asVarsAndVals(data: Record<string, any>[]): [string[], any[][]] {
-        if (data.length === 0) return [[], []]
-        let [first, ...rest] = data
-
-        const variables = [...Object.keys(first)];
-        let values = [...Object.values(first).map((v) => [v])];
-        for (const record of rest) {
-            for (const [i, variable] of variables.entries()) {
-                values[i].push(record[variable]);
-            }
-        }
-        return [variables, values];
-    }
 
     function asCode(variables: string[], values: any[][]): string {
         let result = [];
         for (const [i, variable] of variables.entries()) {
-            result.push(`${variable} = ${values[i][0]}`)
+            if (variable) {
+                result.push(`${variable} = ${values[0][i] !== '' ? values[0][i] : 'None'}`);
+            }
         }
         return result.join('\n');
     }
 
 
-    let data = [
-        {first: 0, second: 1, third: 2},
-        {first: 2, second: 3, third: 4},
-    ];
+    let variables = ['first', 'second', 'third'];
+    let values = [[0, 2, 4], [1, 3, 5]];
 
-    $: [variables, values] = asVarsAndVals(data);
-    $: columnDefs = [...variables.map((name) => ({headerName: name, field: name, sortable: true, editable: true}))];
     $: code = asCode(variables, values);
 
     let pyodide;
@@ -57,7 +28,7 @@
         console.log('pyodide loaded!')
     })
 
-    let counter  = 0;
+    let counter = 0;
     $: {
         console.log(counter);
         counter = counter + 1;
@@ -65,13 +36,12 @@
         if (pyodide) {
             pyodide.runPython(code);
         }
-        console.log(data);
     }
 </script>
 
 <div id="container">
     <div id="grid">
-        <AgGrid bind:data={data} {columnDefs}/>
+        <Grid bind:variables bind:values/>
     </div>
     <div id="editor">
         <CodeMirror bind:value={code} lang={python()}/>
@@ -88,7 +58,7 @@
         width: 70%;
     }
 
-    #editor{
+    #editor {
         display: block;
         flex-grow: 1;
         width: 500px;
